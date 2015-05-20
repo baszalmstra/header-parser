841e055eda298df54fd1c82348b2479dcc6fd8cd
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-05-20 22:20:50 +0200

    Added handler

---
+++ handler.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+// ----------------------------------------------------------------------------------------------------
+
+enum AccessSpecifierType
+{
+    PUBLIC,
+    PROTECTED,
+    PRIVATE
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+typedef std::string Type;
+
+// ----------------------------------------------------------------------------------------------------
+
+struct FieldInfo
+{
+    std::string name;
+    Type type;
+    AccessSpecifierType access_specifier;
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+struct ArgumentInfo
+{
+    std::string name;
+    Type type;
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+struct FunctionInfo
+{
+    std::string name;
+    Type return_type;
+    std::vector<ArgumentInfo> arguments;
+    bool is_const;
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+struct EnumInfo
+{
+    std::string name;
+    std::vector<std::string> enumerators;
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+struct ClassInfo
+{
+    std::string name;
+    std::vector<FunctionInfo> methods;
+    std::vector<FieldInfo> fields;
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+struct StructInfo
+{
+    std::string name;
+    std::vector<FunctionInfo> methods;
+    std::vector<FieldInfo> fields;
+};
+
+// ----------------------------------------------------------------------------------------------------
+
+class Handler
+{
+    virtual void Class(const ClassInfo& info) {}
+    virtual void Struct(const StructInfo& info) {}
+    virtual void Function(const FunctionInfo& info) {}
+    virtual void Field(const FieldInfo& info) {}
+    virtual void Enum(const EnumInfo& info) {}
+};
